dbf3e1a11296115057538314b4f8f316599a2cd2
AdityaPrasad-10 <y.adityaprasad10@gmail.com>
2023-11-21 12:34:57 +0530

    final

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Daytwofirst from './Component/Daytwofirst';
+import Daytwosecond from './Component/Daytwosecond';
+import Day2pah from './Component/Day2pah';
+import Day3CE from './Component/Day3CE';
+import Transformation from './Component/Transformation';
+
+/*import Day4cwhelp1 from './Component/Day4cwhelp1';
+import Day4cwhelp2 from './Component/Day4cwhelp2';
+import Day4cwhelp3 from './Component/Day4cwhelp3'; */
+
+import Day5ce from './Component/Day5ce';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Daytwofirst/>
+      <Daytwosecond/>
+      <Day2pah/>
+      <Day3CE/>
+      <Transformation/>
+      <Day5ce/>
+    </div>
+  );
+}
+
+export default App;
